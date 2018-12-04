34c8c6631f4946738c2deaf75584d7a25ecb73f1
Antonin Houska <ah@cybertec.at>
2018-12-04 16:49:58 +0100

    Added update triggers for the "granted" column of views.

--- pg_permissions--1.0.sql
+++ pg_permissions--1.0.sql
@@ -126,7 +126,7 @@ SELECT obj_type 'SCHEMA' AS object_type,
        n.nspname AS schema_name,
        NULL::text AS object_name,
        NULL::name AS column_name,
-       p.perm::perm_type AS permissions,
+       p.perm::perm_type AS permission,
        has_schema_privilege(r.oid, n.oid, p.perm) AS granted
 FROM pg_catalog.pg_namespace AS n
    CROSS JOIN pg_catalog.pg_roles AS r
@@ -145,7 +145,7 @@ SELECT obj_type 'DATABASE' AS object_type,
     NULL::name AS schema_name,
     NULL::text AS object_name,
     NULL::name AS column_name,
-    p.perm::perm_type AS permissions,
+    p.perm::perm_type AS permission,
     has_database_privilege(r.oid, d.oid, p.perm) AS granted
 FROM pg_catalog.pg_database AS d
    CROSS JOIN pg_catalog.pg_roles AS r
@@ -289,3 +289,145 @@ BEGIN
 
    RETURN QUERY SELECT DISTINCT * FROM findings;
 END;$$;
+
+-- Update triggers.
+CREATE FUNCTION permissions_trigger_func()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+DECLARE
+	db_name	text;
+	cmd	text;
+BEGIN
+	IF NEW.object_type <> OLD.object_type OR
+		NEW.role_name <> OLD.role_name OR
+		NEW.schema_name <> OLD.schema_name OR
+		NEW.object_name <> OLD.object_name OR
+		NEW.column_name <> OLD.column_name OR
+		NEW.permission <> OLD.permission
+	THEN
+		RAISE 'Only the "granted" column can be updated';
+	END IF;
+
+	-- Is there anything to do at all?
+	IF NEW.granted = OLD.granted
+	THEN
+		RETURN NEW;
+	END IF;
+
+	IF OLD.object_type IN ('TABLE', 'VIEW')
+	THEN
+		IF NOT OLD.granted
+		THEN
+			cmd := format('GRANT %s ON %s.%s TO %s',
+				OLD.permission, OLD.schema_name,
+				OLD.object_name, OLD.role_name);
+		ELSE
+			cmd := format('REVOKE %s ON %s.%s FROM %s',
+				OLD.permission, OLD.schema_name,
+				OLD.object_name, OLD.role_name);
+		END IF;
+	ELSIF OLD.object_type = 'COLUMN'
+	THEN
+		IF NOT OLD.granted
+		THEN
+			cmd := format('GRANT %s(%s) ON %s.%s TO %s',
+				OLD.permission, OLD.column_name,
+				OLD.schema_name, OLD.object_name,
+				OLD.role_name);
+		ELSE
+			cmd := format('REVOKE %s(%s) ON %s.%s FROM %s',
+				OLD.permission, OLD.column_name,
+				OLD.schema_name, OLD.object_name,
+				OLD.role_name);
+		END IF;
+	ELSIF OLD.object_type = 'SEQUENCE'
+	THEN
+		IF NOT OLD.granted
+		THEN
+			cmd := format('GRANT %s ON SEQUENCE %s.%s TO %s',
+				OLD.permission, OLD.schema_name,
+				OLD.object_name, OLD.role_name);
+		ELSE
+			cmd := format('REVOKE %s ON SEQUENCE %s.%s FROM %s',
+				OLD.permission, OLD.schema_name,
+				OLD.object_name, OLD.role_name);
+		END IF;
+	ELSIF OLD.object_type = 'FUNCTION'
+	THEN
+		IF NOT OLD.granted
+		THEN
+			cmd := format('GRANT %s ON FUNCTION %s.%s TO %s',
+				OLD.permission, OLD.schema_name,
+				OLD.object_name, OLD.role_name);
+		ELSE
+			cmd := format('REVOKE %s ON FUNCTION %s.%s FROM %s',
+				OLD.permission, OLD.schema_name,
+				OLD.object_name, OLD.role_name);
+		END IF;
+	ELSIF OLD.object_type = 'SCHEMA'
+	THEN
+		IF NOT OLD.granted
+		THEN
+			cmd := format('GRANT %s ON SCHEMA %s TO %s',
+				OLD.permission, OLD.schema_name,
+				OLD.role_name);
+		ELSE
+			cmd := format('REVOKE %s ON SCHEMA %s FROM %s',
+				OLD.permission, OLD.schema_name,
+				OLD.role_name);
+		END IF;
+	ELSIF OLD.object_type = 'DATABASE'
+	THEN
+		db_name := pg_catalog.current_database();
+
+		IF NOT OLD.granted
+		THEN
+			cmd := format('GRANT %s ON DATABASE %s TO %s',
+				OLD.permission, db_name, OLD.role_name);
+		ELSE
+			cmd := format('REVOKE %s ON DATABASE %s FROM %s',
+				OLD.permission, db_name, OLD.role_name);
+		END IF;
+	ELSE
+		RAISE 'Unrecognized object type: %',
+			OLD.object_type;
+	END IF;
+
+	RAISE DEBUG 'cmd: %', cmd;
+	EXECUTE cmd;
+	RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER permissions_trigger
+INSTEAD OF UPDATE ON table_permissions
+FOR EACH ROW EXECUTE PROCEDURE permissions_trigger_func();
+
+CREATE TRIGGER permissions_trigger
+INSTEAD OF UPDATE ON column_permissions
+FOR EACH ROW EXECUTE PROCEDURE permissions_trigger_func();
+
+CREATE TRIGGER permissions_trigger
+INSTEAD OF UPDATE ON view_permissions
+FOR EACH ROW EXECUTE PROCEDURE permissions_trigger_func();
+
+CREATE TRIGGER permissions_trigger
+INSTEAD OF UPDATE ON sequence_permissions
+FOR EACH ROW EXECUTE PROCEDURE permissions_trigger_func();
+
+CREATE TRIGGER permissions_trigger
+INSTEAD OF UPDATE ON function_permissions
+FOR EACH ROW EXECUTE PROCEDURE permissions_trigger_func();
+
+CREATE TRIGGER permissions_trigger
+INSTEAD OF UPDATE ON schema_permissions
+FOR EACH ROW EXECUTE PROCEDURE permissions_trigger_func();
+
+CREATE TRIGGER permissions_trigger
+INSTEAD OF UPDATE ON database_permissions
+FOR EACH ROW EXECUTE PROCEDURE permissions_trigger_func();
+
+CREATE TRIGGER permissions_trigger
+INSTEAD OF UPDATE ON all_permissions
+FOR EACH ROW EXECUTE PROCEDURE permissions_trigger_func();
